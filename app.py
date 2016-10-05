e3ce111d8194dddda06eb4870dc5c0c1f96663d7
ogal7 <ogallager@stuy.edu>
2016-10-05 01:38:57 -0400

    bop to the top

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from flask import Flask, render_template, request
+import csv 
 
 app = Flask(__name__)
 username = "ilovepugs123"
@@ -24,6 +25,20 @@ def auth():
        return "congrats, your password worked"
    else:
        return "na u didn't remember ya password :( "
+
+
+@app.route("/create/", methods = ['POST'])
+def create():
+    #check if user already created
+    with open('logininfo.csv', r) as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if (request.form['user'] == row[0]):
+                return "this username has already been taken!"
+            else:
+                with open('logininfo.csv', w) as csvfile:
+                    info2Enter = request.form['user'] + "," + request.form['pw']
+                    csvwriter.writerow(info2Enter)
 
 if __name__ == '__main__':
     app.run()
